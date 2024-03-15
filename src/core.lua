126a8088c91336e42636a661b149b2d78be238b0
skuzzis <andreieurt@gmail.com>
2024-03-15 20:50:36 +0200

    upload(src): First Upload

---
+++ src/core.lua
@@ -0,0 +1,38 @@
+local messages = {}
+
+events:on("OnPluginLoad", function()
+    for i=1,config:FetchArraySize("welcomemessage.messages") do
+        local message = config:Fetch("welcomemessage.messages["..(i-1).."]")
+        table.insert(messages, message)
+    end
+end)
+
+events:on("OnPlayerSpawn", function(playerid)
+    local player = GetPlayer(playerid)
+    if not player then return end
+
+    if player:IsFirstSpawn() == 1 then
+        for i=1,#messages do 
+            local msg = messages[i]
+            msg = msg:gsub("{PLAYERNAME}", player:GetName()):gsub("{PLAYERS}", tostring(playermanager:GetPlayerCount())):gsub("{MAXPLAYERS}", tostring(playermanager:GetPlayerCap())):gsub("{MAP}", server:GetMap())
+
+            player:SendMsg(MessageType.Chat, string.format("%s %s", config:Fetch("welcomemessage.prefix"), msg))
+        end
+    end
+end)
+
+function GetPluginAuthor()
+    return "Swiftly Solution"
+end
+
+function GetPluginVersion()
+    return "v1.0.0"
+end
+
+function GetPluginName()
+    return "[Swiftly] Welcome Messages"
+end
+
+function GetPluginWebsite()
+    return "https://github.com/swiftly-solution/swiftly_welcomemessage"
+end
